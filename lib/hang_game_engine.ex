3cedbd04574078325e63413cba6b0a239601b7bc
lucho870601 <lucho870601@gmail.com>
2013-11-22 19:32:57 +0100

    Fixed wrong win condition and added count of wrong attempts.

--- lib/hang_game_engine.ex
+++ lib/hang_game_engine.ex
@@ -2,7 +2,7 @@ defmodule HangGameEngine do
 
   def guessed?(secret, tries) do
     spaceless_secret = :sets.del_element(32, :sets.from_list(secret))
-    spaceless_secret == :sets.from_list(tries)
+    :sets.is_subset(spaceless_secret, :sets.from_list(tries))
   end
 
   def currently_guessed(secret, tries) do
@@ -14,6 +14,12 @@ defmodule HangGameEngine do
         '_'
       end
     end |> :binary.list_to_bin
+  end
+
+  def wrong_tryouts(secret, tries) do
+    spaceless_secret = :sets.del_element(32, :sets.from_list(secret))
+    tries_set = :sets.from_list(tries)
+    :sets.subtract(tries_set, spaceless_secret) |> :sets.size
   end
 
   def generate_secret(options) do
--- test/hang_game_engine_test.exs
+++ test/hang_game_engine_test.exs
@@ -1,25 +1,33 @@
 defmodule HangGameEngineTest do
   use ExUnit.Case
 
+  import HangGameEngine
+
   test "guessed entire secret" do
-    assert HangGameEngine.currently_guessed('happy tree friends', 'hapytrefinds') == "happy tree friends"
+    assert currently_guessed('happy tree friends', 'hapytrefinds') == "happy tree friends"
   end
 
   test "partially guessed secret" do
-    assert HangGameEngine.currently_guessed('happy tree friends', 'haptre') == "happ_ tree _r_e___"
+    assert currently_guessed('happy tree friends', 'haptre') == "happ_ tree _r_e___"
   end
 
   test "not guessed at all" do
-    assert HangGameEngine.currently_guessed('happy tree friends', '') == "_____ ____ _______"
+    assert currently_guessed('happy tree friends', '') == "_____ ____ _______"
   end
 
   test "guessed?" do
-    assert HangGameEngine.guessed?('happy', 'hap') == false
-    assert HangGameEngine.guessed?('happy tree friends', 'hap') == false
-    assert HangGameEngine.guessed?('happy tree friends', 'hapytrefinds') == true
+    assert guessed?('happy', 'hapxzjk') == false
+    assert guessed?('happy tree friends', 'hapx') == false
+    assert guessed?('happy tree friends', 'hapxytrefinds') == true
   end
 
   test "generate secret" do
-    assert HangGameEngine.generate_secret(['a']) == 'a'
+    assert generate_secret(['a']) == 'a'
+  end
+
+  test "count wrong tryouts" do
+    assert wrong_tryouts('happy tree', 'jhapqwer') == 3
+    assert wrong_tryouts('happy tree', 'qwzxl') == 5
+    assert wrong_tryouts('happy tree', 'hap') == 0
   end
 end
